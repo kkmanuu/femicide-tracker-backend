e8610e1ffe6291a364cae55aae08a5c8f016c7c8
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-08 19:40:42 +0300

    feat: add endpoint to fetch metadata for counties, perpetrators, and weapons

--- controllers/metadataController.js
+++ controllers/metadataController.js
@@ -1,20 +1,26 @@
-const pool = require('../config/db');
+const pool = require("../config/db");
 
 exports.getMetadata = async (req, res, next) => {
   try {
     // Get counties
-    const [counties] = await pool.query('SELECT name FROM counties ORDER BY name');
-    
+    const [counties] = await pool.query(
+      "SELECT name FROM counties ORDER BY name"
+    );
+
     // Get perpetrators
-    const [perpetrators] = await pool.query('SELECT type FROM perpetrators ORDER BY type');
-    
+    const [perpetrators] = await pool.query(
+      "SELECT type FROM perpetrators ORDER BY type"
+    );
+
     // Get weapons
-    const [weapons] = await pool.query('SELECT name FROM weapons ORDER BY name');
-    
+    const [weapons] = await pool.query(
+      "SELECT name FROM weapons ORDER BY name"
+    );
+
     res.json({
-      counties: counties.map(c => c.name),
-      perpetrators: perpetrators.map(p => p.type),
-      weapons: weapons.map(w => w.name)
+      counties: counties.map((c) => c.name),
+      perpetrators: perpetrators.map((p) => p.type),
+      weapons: weapons.map((w) => w.name),
     });
   } catch (err) {
     next(err);
